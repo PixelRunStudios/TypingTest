cd75add6e38523e6662cfff668ac398824033764
Alexander Guo <alexanderguo99@gmail.com>
2014-09-20 00:29:24 -0400

    added main stuff

--- src/com/github/picrazy2/TypingTest/Main.java
+++ src/com/github/picrazy2/TypingTest/Main.java
@@ -1,59 +1,99 @@
 package com.github.picrazy2.TypingTest;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main{
 
 
 	static long startTime = 0;
-	static long endTime = 0;
-	static long timePassed = 0;
+	static long currentTime = 0;
 	static Random random = new Random();
+
+	static ArrayList<String> cont = new ArrayList<String>();
+	static ArrayList<String> fullCont = new ArrayList<String>();
+
+	static ArrayList<String> order = new ArrayList<String>();
 
 	public static void main(String[] args){
 
 
 		// TODO Auto-generated method stub
 		String line = null;
-		String content = "";
+		int correctCounter = 0;
 
-		/*File file = new File("bank.txt");
+		File file = new File("bank.txt");
 		try{
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			line = reader.readLine();
 			while(line!=null){
-				content += line + "\n";
+				cont.add(line);
+				fullCont.add(line);
 				line = reader.readLine();
 			}
 		}
 		catch(IOException e){
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 		System.out.println("1 minute starts after pressing 1");
 		BufferedReader systemIn = new BufferedReader(new InputStreamReader(System.in));
-		boolean start = false;
+		boolean start = true;
+		int counter = 0;
+
 		while(true){
 			try{
 				String input = systemIn.readLine();
-				if(input.equals("break")){
+				if(input.equals("quit")){
 					break;
+				}
+				else if(input.equals("1") && start){
+					makeWords();
+					start = false;
+					startTime = System.currentTimeMillis();
+					for(int i = counter; i<counter+5; i++){
+						System.out.print(order.get(i%order.size()) + " ");
+					}
+					System.out.println();
+				}else if(!start){
+					for(int i = counter; i<counter+5; i++){
+						System.out.print(order.get(i%order.size()) + " ");
+					}
+					System.out.println();
+					if(input.equalsIgnoreCase(order.get(counter%order.size()))){
+						correctCounter++;
+						currentTime = System.currentTimeMillis();
+						if(currentTime-startTime>=1000){
+							System.out.println("Time up! Score: "+correctCounter);
+							break;
+
+						}
+					}
 				}
 			}
 			catch(IOException e){
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+
+			counter++;
 		}
 
-		startTime = System.currentTimeMillis();
 
-		endTime = System.currentTimeMillis();
-		timePassed = endTime - startTime;
 
+	}
+
+	public static void makeWords(){
+		while(cont.size()!=0){
+			int temp = random.nextInt(cont.size());
+			order.add(cont.get(temp));
+			cont.remove(temp);
+		}
 	}
 
 }
